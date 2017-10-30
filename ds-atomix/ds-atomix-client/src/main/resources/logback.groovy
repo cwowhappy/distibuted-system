8fd28b47cf6f616c81044c93ce2bc4ecc99a0cde
lixiaoyi <lixiaoyi@didichuxing.com>
2017-10-30 15:58:24 +0800

    配置日志记录器

---
+++ ds-atomix/ds-atomix-client/src/main/resources/logback.groovy
@@ -0,0 +1,29 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.filter.ThresholdFilter
+
+import static ch.qos.logback.classic.Level.*
+
+def USER_HOME = System.getProperty("user.home")
+def APP_NAME = "ds-atomix-client"
+
+appender("STDOUT", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+    }
+}
+
+appender("dailyRollingFileAppender", RollingFileAppender) {
+    file = "logs/${APP_NAME}.log"
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "${APP_NAME}.log.%d{yyyy-MM-dd}"
+        maxHistory = 30
+    }
+    filter(ThresholdFilter) {
+        level = INFO
+    }
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+    }
+}
+
+root(DEBUG, ["STDOUT", "dailyRollingFileAppender"])
